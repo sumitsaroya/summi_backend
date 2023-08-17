1957dc8d83f5d4d825c59dcc3e086da719202764
workpanel <141266921+workpanel@users.noreply.github.com>
2023-08-17 16:40:40 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,42 +6,17 @@ const {
 } = require("../controllers/authController");
 const { resetPasswordController } = require('../controllers/authController');
 
-const router = express.Router();
 
-// Simulated session tracking with in-memory storage
-const activeSessions = {};
+const router = express.Router();
 
 // REGISTER
 router.post("/register", registerController);
 
 // LOGIN
-router.post("/login", (req, res) => {
-  // Perform login logic
-  // After successful login, create and store a session token
-  const userId = "user123"; // Replace with actual user ID
-  const sessionToken = "some_unique_token"; // Generate a session token
-  if (!activeSessions[userId]) {
-    activeSessions[userId] = [];
-  }
-  activeSessions[userId].push(sessionToken);
+router.post("/login", loginController);
 
-  // Send response
-  res.status(200).json({ message: "Login successful", sessionToken });
-});
-
-// LOGOUT from ALL DEVICES
-router.post("/logout-all-devices", (req, res) => {
-  const { userId } = req.body;
-
-  if (userId && activeSessions[userId]) {
-    // Invalidate all sessions for the user
-    activeSessions[userId] = [];
-
-    res.status(200).json({ message: "Logged out from all devices" });
-  } else {
-    res.status(400).json({ message: "User not found" });
-  }
-});
+// LOGOUT
+router.post("/logout", logoutController);
 
 // RESET PASSWORD
 router.post("/reset-password", resetPasswordController);
